05d2aa45f1f2cc985dc648c0f16e32844bd9a1e9
moszie248 <moszie248@hotmail.com>
2020-06-24 00:10:22 +0700

    1st

---
+++ layouts/default.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <header class="main_h">
+      <div class="row">
+        <a class="logo" href="#">M.2DEV</a>
+
+        <div class="mobile-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <nav>
+          <ul>
+            <li><a href=".profile">Profile</a></li>
+            <li><a href=".gallery">Gallery</a></li>
+            <li><a href=".portfolio">Portfolio</a></li>
+            <li><a href=".contact">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+      <!-- / row -->
+    </header>
+    <Nuxt />
+  </div>
+</template>
+<script>
+import $ from "jquery";
+export default {
+  mounted() {
+    // Sticky Header
+    $(window).scroll(function() {
+      if ($(window).scrollTop() > 100) {
+        $(".main_h").addClass("sticky");
+      } else {
+        $(".main_h").removeClass("sticky");
+      }
+    });
+
+    // Mobile Navigation
+    $(".mobile-toggle").click(function() {
+      if ($(".main_h").hasClass("open-nav")) {
+        $(".main_h").removeClass("open-nav");
+      } else {
+        $(".main_h").addClass("open-nav");
+      }
+    });
+
+    $(".main_h li a").click(function() {
+      if ($(".main_h").hasClass("open-nav")) {
+        $(".navigation").removeClass("open-nav");
+        $(".main_h").removeClass("open-nav");
+      }
+    });
+
+    // navigation scroll lijepo radi materem
+    $("nav a").click(function(event) {
+      const id = $(this).attr("href");
+      const offset = 70;
+      const target = $(id).offset().top - offset;
+      $("html, body").animate(
+        {
+          scrollTop: target
+        },
+        500
+      );
+      event.preventDefault();
+    });
+  }
+};
+</script>
+<style>
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,180 @@
+<template>
+  <div class="container">
+    <div class="container">
+      <div class="hero">
+        <h1><span>I'm a cool</span><br />Navigation</h1>
+
+        <div class="mouse">
+          <span></span>
+        </div>
+      </div>
+
+      <div class="row content">
+        <h1 class="profile">It's My.</h1>
+        <div class="grid-profile">
+          <h2>ssss</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum,
+            recusandae, at, labore velit eligendi amet nobis repellat natus
+            sequi sint consectetur excepturi doloribus vero provident
+            consequuntur accusamus quisquam nesciunt cupiditate soluta alias
+            illo et deleniti voluptates facilis repudiandae similique dolore
+            quaerat architecto perspiciatis officiis dolor ullam expedita
+            suscipit neque minima rem praesentium inventore ab officia quos
+            dignissimos esse quam placeat iste porro eius! Minus, aspernatur
+            nesciunt consectetur. Sit, eius, itaque, porro, beatae impedit
+            officia tenetur reiciendis autem vitae a quae ipsam repudiandae odio
+            dolorum quaerat asperiores possimus corporis optio animi quisquam
+            laboriosam nihil quam voluptatum quidem veritatis iste culpa iure
+            modi perspiciatis recusandae ipsa libero officiis aliquam doloremque
+            similique id quasi atque distinctio enim sapiente ratione in quia
+            eum perferendis earum blanditiis. Nobis, architecto, veniam
+            molestias minus iste necessitatibus est ab in earum ratione eveniet
+            soluta molestiae sed illo nostrum nemo debitis. Minus, quod totam
+            aliquam ea asperiores fugit quaerat excepturi dolores ratione
+            numquam consequatur id unde alias provident vero incidunt
+            exercitationem similique consequuntur hic possimus? Fuga, eveniet
+            quaerat inventore corporis laborum eligendi enim soluta obcaecati
+            aliquid veritatis provident amet laudantium est quisquam dolore
+            exercitationem modi? Distinctio, pariatur, ab velit praesentium
+            vitae quidem consequatur deleniti recusandae odit officiis. Quidem,
+            cupiditate.
+          </p>
+        </div>
+
+        <h1 class="gallery">Look at me.</h1>
+        <div class="grid-gallery">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum,
+            recusandae, at, labore velit eligendi amet nobis repellat natus
+            sequi sint consectetur excepturi doloribus vero provident
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum,
+            recusandae, at, labore velit eligendi amet nobis repellat natus
+            sequi sint consectetur excepturi doloribus vero provident
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum,
+            recusandae, at, labore velit eligendi amet nobis repellat natus
+            sequi sint consectetur excepturi doloribus vero provident
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum,
+            recusandae, at, labore velit eligendi amet nobis repellat natus
+            sequi sint consectetur excepturi doloribus vero provident
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum,
+            recusandae, at, labore velit eligendi amet nobis repellat natus
+            sequi sint consectetur excepturi doloribus vero provident
+          </p>
+        </div>
+
+        <h1 class="portfolio">Level up.</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum,
+          recusandae, at, labore velit eligendi amet nobis repellat natus sequi
+          sint consectetur excepturi doloribus vero provident consequuntur
+          accusamus quisquam nesciunt cupiditate soluta alias illo et deleniti
+          voluptates facilis repudiandae similique dolore quaerat architecto
+          perspiciatis officiis dolor ullam expedita suscipit neque minima rem
+          praesentium inventore ab officia quos dignissimos esse quam placeat
+          iste porro eius! Minus, aspernatur nesciunt consectetur. Sit, eius,
+          itaque, porro, beatae impedit officia tenetur reiciendis autem vitae a
+          quae ipsam repudiandae odio dolorum quaerat asperiores possimus
+          corporis optio animi quisquam laboriosam nihil quam voluptatum quidem
+          veritatis iste culpa iure modi perspiciatis recusandae ipsa libero
+          officiis aliquam doloremque similique id quasi atque distinctio enim
+          sapiente ratione in quia eum perferendis earum blanditiis. Nobis,
+          architecto, veniam molestias minus iste necessitatibus est ab in earum
+          ratione eveniet soluta molestiae sed illo nostrum nemo debitis. Minus,
+          quod totam aliquam ea asperiores fugit quaerat excepturi dolores
+          ratione numquam consequatur id unde alias provident vero incidunt
+          exercitationem similique consequuntur hic possimus? Fuga, eveniet
+          quaerat inventore corporis laborum eligendi enim soluta obcaecati
+          aliquid veritatis provident amet laudantium est quisquam dolore
+          exercitationem modi? Distinctio, pariatur, ab velit praesentium vitae
+          quidem consequatur deleniti recusandae odit officiis. Quidem,
+          cupiditate.
+        </p>
+        <h1 class="contact">Chat to me.</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum,
+          recusandae, at, labore velit eligendi amet nobis repellat natus sequi
+          sint consectetur excepturi doloribus vero provident consequuntur
+          accusamus quisquam nesciunt cupiditate soluta alias illo et deleniti
+          voluptates facilis repudiandae similique dolore quaerat architecto
+          perspiciatis officiis dolor ullam expedita suscipit neque minima rem
+          praesentium inventore ab officia quos dignissimos esse quam placeat
+          iste porro eius! Minus, aspernatur nesciunt consectetur. Sit, eius,
+          itaque, porro, beatae impedit officia tenetur reiciendis autem vitae a
+          quae ipsam repudiandae odio dolorum quaerat asperiores possimus
+          corporis optio animi quisquam laboriosam nihil quam voluptatum quidem
+          veritatis iste culpa iure modi perspiciatis recusandae ipsa libero
+          officiis aliquam doloremque similique id quasi atque distinctio enim
+          sapiente ratione in quia eum perferendis earum blanditiis. Nobis,
+          architecto, veniam molestias minus iste necessitatibus est ab in earum
+          ratione eveniet soluta molestiae sed illo nostrum nemo debitis. Minus,
+          quod totam aliquam ea asperiores fugit quaerat excepturi dolores
+          ratione numquam consequatur id unde alias provident vero incidunt
+          exercitationem similique consequuntur hic possimus? Fuga, eveniet
+          quaerat inventore corporis laborum eligendi enim soluta obcaecati
+          aliquid veritatis provident amet laudantium est quisquam dolore
+          exercitationem modi? Distinctio, pariatur, ab velit praesentium vitae
+          quidem consequatur deleniti recusandae odit officiis. Quidem,
+          cupiditate.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "default"
+};
+</script>
+
+<style>
+/* @import '../assets/css/mystyle.css'; */
+/* .container {
+  max-width: 1400px;
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+} */
+</style>
